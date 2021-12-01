1cb376eedb1cf8b377d6e76801ed99f904795d8b
Taneto39 <65605866+Taneto39@users.noreply.github.com>
2021-12-02 01:10:36 +0700

    debug symbol check

--- ThePassword.py
+++ ThePassword.py
@@ -1,15 +1,15 @@
-l, u, d, s = 0, 0, 0, 0
+lo, u, d, s = 0, 0, 0, 0
 x = input()
 for i in x:
     if i.islower():
-        l += 1
+        lo += 1
     if i.isupper():
         u += 1
     if i.isdigit():
         d += 1
-    if not (i.isdigit() and i.isalpha()):
+    if not i.isdigit() and not i.isalpha():
         s += 1
-if l >= 1 and u >= 1 and d >= 1 and s >= 1 and 3 <= len(x) <= 20:
+if lo >= 1 and u >= 1 and d >= 1 and s >= 1 and 3 <= len(x) <= 20:
     print("Valid")
 else:
     print("Invalid")
